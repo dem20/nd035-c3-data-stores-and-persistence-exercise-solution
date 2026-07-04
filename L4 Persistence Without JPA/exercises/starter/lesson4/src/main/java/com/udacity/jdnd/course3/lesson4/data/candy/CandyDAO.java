1bb8ea40cd933b481ed84a83641f131de2631b19
dem20 <de_ni_se1@hotmail.com>
2026-07-03 22:06:13 -0600

    DAO impl get a list of all candies, add candy to delivery, find candies per delivery

--- L4 Persistence Without JPA/exercises/starter/lesson4/src/main/java/com/udacity/jdnd/course3/lesson4/data/candy/CandyDAO.java
+++ L4 Persistence Without JPA/exercises/starter/lesson4/src/main/java/com/udacity/jdnd/course3/lesson4/data/candy/CandyDAO.java
@@ -2,7 +2,7 @@ package com.udacity.jdnd.course3.lesson4.data.candy;
 import java.util.List;
 
 public interface CandyDAO {
-    List<CandyData> list();
+    List<CandyData> getAllCandy();
    void addToDelivery(Long candyId, Long deliveryid);
    List<CandyData> findByDelivery(Long deliveryId);
 } 
---
+++ L4 Persistence Without JPA/exercises/starter/lesson4/src/main/java/com/udacity/jdnd/course3/lesson4/data/candy/CandyDAOImpl.java
@@ -0,0 +1,59 @@
+package com.udacity.jdnd.course3.lesson4.data.candy;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+@Repository
+@Transactional
+public class CandyDAOImpl implements CandyDAO {
+
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final String CANDY_ID = "candyId";
+    private static final String DELIVERY_ID = "deliveryId";
+
+    private static final String SELECT_ALL_CANDY =
+            "SELECT * FROM candy";
+
+    private static final String INSERT_DELIVERY =
+            "INSERT INTO candy_delivery (candy_id, delivery_id) " +
+            "VALUES (:" + CANDY_ID + ", :" + DELIVERY_ID + ")";
+
+    private static final String FIND_CANDY_BY_DELIVERY =
+            "SELECT c.* FROM candy_delivery AS cd " +
+            "JOIN candy AS c ON c.id = cd.candy_id " +
+            "WHERE cd.delivery_id = :" + DELIVERY_ID;
+
+     private static final RowMapper<CandyData> candyDataRowMapper =
+            new BeanPropertyRowMapper<>(CandyData.class);
+
+    @Override
+    public List<CandyData> getAllCandy() {
+        return jdbcTemplate.query(SELECT_ALL_CANDY, candyDataRowMapper);
+    }
+
+    @Override
+    public void addToDelivery(Long candyId, Long deliveryid) {
+        jdbcTemplate.update(INSERT_DELIVERY,
+                new MapSqlParameterSource()
+                        .addValue(CANDY_ID, candyId)
+                        .addValue(DELIVERY_ID, deliveryid));
+    }
+
+    @Override
+    public List<CandyData> findByDelivery(Long deliveryId) {
+        return jdbcTemplate.query(FIND_CANDY_BY_DELIVERY,
+                new MapSqlParameterSource()
+                        .addValue(DELIVERY_ID, deliveryId),
+                candyDataRowMapper);
+    }
+    
+}
